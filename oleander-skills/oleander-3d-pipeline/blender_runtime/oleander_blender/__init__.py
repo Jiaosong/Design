96cd3f6688a8d093c94b76cc05c1a68272617c9f
Jiaosong <dy128764@gmail.com>
2026-09-01 22:09:07 +0800

    feat(blender): register angular datum workbench

--- oleander-skills/oleander-3d-pipeline/blender_runtime/oleander_blender/__init__.py
+++ oleander-skills/oleander-3d-pipeline/blender_runtime/oleander_blender/__init__.py
@@ -4,7 +4,7 @@ bl_info = {
     "version": (0, 2, 0),
     "blender": (5, 1, 0),
     "location": "View3D > Sidebar > OLEANDER",
-    "description": "Governed identity, semantics, configurations, relation/dependency/diff, measurement, BOM, audit and manifest workbench for OLEANDER 3D",
+    "description": "Governed identity, semantics, configurations, relation/dependency/diff, measurement, datum, BOM, audit and manifest workbench for OLEANDER 3D",
     "category": "3D View",
 }
 
@@ -29,6 +29,8 @@ from . import measurement_system as _measurement_system
 from .measurement_atomic import install_atomic_quantize
 from .measurement_system import OPERATOR_CLASSES as MEASUREMENT_OPERATOR_CLASSES
 from .measurement_system import PANEL_CLASSES as MEASUREMENT_PANEL_CLASSES
+from .angular_datum import OPERATOR_CLASSES as ANGULAR_DATUM_OPERATOR_CLASSES
+from .angular_datum import PANEL_CLASSES as ANGULAR_DATUM_PANEL_CLASSES
 from .workbench_ops import CLASSES as WORKBENCH_OPERATOR_CLASSES
 from .configuration_ops import CLASSES as CONFIGURATION_CLASSES
 from .bom import CLASSES as BOM_CLASSES
@@ -51,6 +53,7 @@ OPERATOR_CLASSES = (
     *RELATION_OPERATOR_CLASSES,
     *RELATION_APPLY_OPERATOR_CLASSES,
     *MEASUREMENT_OPERATOR_CLASSES,
+    *ANGULAR_DATUM_OPERATOR_CLASSES,
     *WORKBENCH_OPERATOR_CLASSES,
     *CONFIGURATION_CLASSES,
     *BOM_CLASSES,
@@ -63,6 +66,7 @@ PANEL_CLASSES = (
     *RELATION_PANEL_CLASSES,
     *RELATION_APPLY_PANEL_CLASSES,
     *MEASUREMENT_PANEL_CLASSES,
+    *ANGULAR_DATUM_PANEL_CLASSES,
 )
 
 
