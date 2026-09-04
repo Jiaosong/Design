83f2127d846f27806a1b5243bffd36d18082c1b0
Jiaosong <dy128764@gmail.com>
2026-09-05 06:57:59 +0800

    C04 web: use direct authority thumbnail carrier for R06

--- 05-cases/c04-qingjiang-stone-book/web-v1.12-game-line/assets/qj_r06_landscape_keep_v11_data.js
+++ 05-cases/c04-qingjiang-stone-book/web-v1.12-game-line/assets/qj_r06_landscape_keep_v11_data.js
@@ -1,1 +1,1 @@
-window.C04_QJD_R06_DATA='https://drive.google.com/uc?export=view&id=1Md7iDG0pzweLhjZKuMylgP5x8SIe1DwQ';
+window.C04_QJD_R06_DATA='https://drive.google.com/thumbnail?id=1Md7iDG0pzweLhjZKuMylgP5x8SIe1DwQ&sz=w1920';
